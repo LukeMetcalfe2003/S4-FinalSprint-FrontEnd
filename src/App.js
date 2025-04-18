18f8b1de50c37048c9e8982d2979c2b94028bdda
Morgan Browne <morgan.browne@keyin.com>
2025-04-18 09:58:24 -0230

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Cities from './pages/City';
+import "./App.css";
+import Home from './pages/Home';
+import Airport from './pages/Airport';
+import AirportDetails from './pages/AirportDetails';
+import Passenger from './pages/Passenger';
+import Aircraft from './pages/Aircraft';
+import Flight from './pages/Flight';
+import Gate from './pages/Gate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="top">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/airports" element={<Airport />} />
+          <Route path="/airports/:id" element={<AirportDetails />} />
+          <Route path="/passengers" element={<Passenger />} />
+          <Route path="/aircrafts" element={<Aircraft />} />
+          <Route path="/flights" element={<Flight />} />
+          <Route path="/gates" element={<Gate />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
